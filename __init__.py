b1a09c0c46694405fefc528e4e16e087ea55f0f8
Zach Mueller <zach.a.mueller@gmail.com>
2015-05-12 17:27:39 -0500

    BASIC but, JS implemented, needs visual work, some small tweaks such as ignoring case when removing, etc..
    to add: Redis backend
    	Visual update
    	More actions/buttons/etc..

--- __init__.py
+++ __init__.py
@@ -1,43 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-#todos class
-class todos(object):
-    todo = ["Feed dogs", "Finish laundry", "Do Homework", "Call vet", "Get drycleaning"]
-    length = len(todo)
-    
-    def update(self):
-        self.length = len(self.todo)
-    
-    def add(self, msg):
-        self.todo.append(msg)
-        self.update()
-    
-    def remove(self, x):
-        self.todo.remove(x)
-        self.update()
-        
-mylist = todos()
-#Finish todo class
+
 
 @app.route('/', methods =['GET', 'POST'])
 def todoList():
-    try:
-        
-        if request.method == 'POST':
-            if request.form['toAdd'] != "Put a new todo here!":
-                add = request.form['toAdd']
-                mylist.add(add)
-                print "Adding!"
-            elif request.form['toRemove'] != "Put a done todo here!":
-                rem = request.form['toRemove']
-                mylist.remove(rem)
-                print "Removing!"
-            return redirect(url_for("todoList"))
-        else:
-            return render_template("index.html", mylist=mylist)
-    except Exception, e:
-        return str(e)
+    return render_template("index.html")
+    
 
 @app.route('/about/')
 def about():
